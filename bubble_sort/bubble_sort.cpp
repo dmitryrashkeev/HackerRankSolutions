80758a7ed48f57168d94cd927c0c44a6bcca4e5c
Dmitry Rashkeev <rashkeev@gmail.com>
2017-03-13 07:15:23 -0400

    Bubblesort now counts comparisons and swaps.

--- bubble_sort/bubble_sort.cpp
+++ bubble_sort/bubble_sort.cpp
@@ -1,36 +1,49 @@
 #include <iostream>
 #include <vector>
-#include <iterator>
 
 
-template <typename T>
-void swap(T& a, T& b) {
+
+template <typename T, typename C>
+void swap(T& a, T& b, C& counter) {
+	++counter;
 	T c = a;
 	a = b;
 	b = c;
 }
 
+template <typename T, typename C>
+bool compare(const T& a, const T& b, C& counter, ) {
+	++counter;
+	if (a > b) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
 template <typename Iter>
-int bubble_sort(Iter begin, Iter end) {
+std::pair<int, int> bubble_sort(Iter begin, Iter end) {
 	int numSwaps = 0, prevNumSwaps = 0;
+	int numComps = 0;
 	auto endpoint = end - 1;
 
 	do {
 		prevNumSwaps = numSwaps;
 		for (auto j = begin; j != endpoint; ++j) {
 
-			if (*j > *(j + 1)) {
-				swap(*j, *(j + 1));
-				++numSwaps;
+			if (compare(*j, *(j+1), numComps)) {
+				swap(*j, *(j + 1), numSwaps);
 			}
 		}
 	} while (numSwaps > prevNumSwaps);
 
-	for (auto i = begin; i < end; ++i) {
-		std::cout << *i << " ";
-	}
-	std::cout << std::endl;
-	return numSwaps;
+	//for (auto i = begin; i < end; ++i) {
+	//	std::cout << *i << " ";
+	//}
+	//std::cout << std::endl;
+
+	return std::make_pair(numSwaps, numComps);
 }
 
 
@@ -41,8 +54,8 @@ int main() {
 	for (auto i = 0; i < n; ++i) {
 		std::cin >> a[i];
 	}
-	auto swaps = bubble_sort(a.begin(),a.end());
-	std::cout << "Array is sorted in " << swaps << " swaps." << std::endl;
+	auto result = bubble_sort(a.begin(),a.end());
+	std::cout << "Array is sorted in " << result.first << " swaps with " << result.second << " comparisons" << std::endl;
 	std::cout << "First Element: " << a[0] << std::endl;
 	std::cout << "Last Element: " << *(a.end()-1) << std::endl;
 
